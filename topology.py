101535880f3c58a0eef2923eb409fcc45cdd1cb0
Jean-Luc Watson <jlwatson@stanford.edu>
2018-05-21 23:56:03 +0000

    moved filenames in root

--- topology.py
+++ topology.py
@@ -8,123 +8,102 @@ import sys
 
 from collections import defaultdict
 from subprocess import Popen
+import math
+import struct
+import socket
 
+import matplotlib.pyplot as plt 
 
 RNG_SEED = 0xBAEF
 
+k = 24
+p = k / 2
+L = 2
 
-def generate_topology(n_servers, n_switches, n_ports, debug=False):
 
-    if debug:
-        random.seed(RNG_SEED)
-
-    sys.stdout.write("Generating Fellyjish topology: %d servers, %d switches, %d ports per switch..." % (n_servers, n_switches, n_ports))
-
-    topo = {}
-
+def generate_topology(n_servers, k=24, L=2, debug=False):
     G = nx.Graph()
-    topo["graph"] = G
-    topo["n_ports"] = n_ports
-
-    topo["n_hosts"] = n_servers
-    for s in range(n_servers):
-        G.add_node('h'+str(s), ip = '10.0.' + str(s) + '.1')
-
-    topo["n_switches"] = n_switches
+    topo = {}
     outport_mappings = {}
-    open_ports = [n_ports] * n_switches 
+    switch_to_ip = {}
+    host_to_ip = {}
+    topo["graph"] = G
+    topo["n_ports"] = k
+    topo["n_hosts"] = n_servers
+    topo["outport_mappings"] = outport_mappings
+    topo["switch_to_ip"] = switch_to_ip
+    topo["host_to_ip"] = host_to_ip
 
-    for sw in range(n_switches):
-        curr_switch = 's'+str(sw)
-        G.add_node(curr_switch)
+    p = k / 2
 
-        i = sw
-        while i < n_servers:
-            G.add_edge('h'+str(i), curr_switch)
-            outport_mappings[(curr_switch, 'h'+str(i))] = open_ports[sw]
-            outport_mappings[('h'+str(i), curr_switch)] = 1
-            i += n_switches
-            open_ports[sw] -= 1
+    location_bit_length = int(2 * math.ceil(math.log(p, 2))) + 1
+    padding = 16 - location_bit_length
+    n_switches_per_layer = 2 * p ** L
+    num_servers_per_switch = n_servers / n_switches_per_layer
 
-    start_open_ports = copy.deepcopy(open_ports)
-    
-    topo['outport_mappings'] = outport_mappings
-    # randomly link the remaining open ports
-    links = defaultdict(list)
-    while sum(open_ports) > 1:
-        open_switches = [x for x in range(n_switches) if open_ports[x] > 0]
-        if len(open_switches) == 1: # special case with two ports remaining on same switch
-            curr = open_switches[0]
-            if open_ports[curr] >= 2:
-                other_switches = [s for s in range(n_switches) if s != curr]
-                x = 's'+str(random.choice(other_switches))
-                y = random.choice(list(nx.all_neighbors(G, x)))
-                G.remove_edge(x, y)
-                x_port = outport_mappings.pop((x, y))
-                y_port = outport_mappings.pop((y, x))
-                G.add_edge(x, 's'+str(curr))
-                G.add_edge(y, 's'+str(curr))
-                outport_mappings[(x, 's'+str(curr))] = x_port
-                outport_mappings[('s'+str(curr), x)] = open_ports[curr]
-                outport_mappings[(y, 's'+str(curr))] = y_port
-                outport_mappings[('s'+str(curr), y)] = open_ports[curr] - 1
-                open_ports[curr] -= 2
-                continue
+    total_switches = n_switches_per_layer * L + n_switches_per_layer / 2
 
-        start_over = False
-        while True:
-            x = random.choice(open_switches)
-            x_name = 's'+str(x)
-            unconnected_switches = [s for s in open_switches if ('s'+str(s) not in list(G.neighbors(x_name)) and s != x)]
-            if len(unconnected_switches) == 0:
-                no_new_links = True
-                for os in open_switches:
-                    for os2 in open_switches:
-                        if os != os2:
-                            no_new_links = no_new_links and 's'+str(os2) in list(G.neighbors('s'+str(os)))
-                if no_new_links:
-                    start_over = True
-                    break
-            else:
-                break
-        
-        if start_over:
-            open_ports = copy.deepcopy(start_open_ports)
-            G = nx.create_empty_copy(G)
-            continue
+    open_ports = [k] * total_switches
 
-        y = random.choice(unconnected_switches)
-        open_ports[x] -= 1
-        open_ports[y] -= 1
-        G.add_edge('s'+str(x), 's'+str(y))
-        outport_mappings[('s'+str(x), 's'+str(y))] = open_ports[x] + 1
-        outport_mappings[('s'+str(y), 's'+str(x))] = open_ports[y] + 1
+    topo["n_switches"] = total_switches 
 
-    sys.stdout.write(" done\n")
+
+    for loc in range(2 ** location_bit_length):
+        for c in range(num_servers_per_switch):
+            ip = socket.inet_ntoa(struct.pack('!L', 10 << 24 | loc << 10 | (c + 1)))
+            host_num = loc * num_servers_per_switch + c
+            host_to_ip[host_num] = ip
+            G.add_node('h'+str(host_num), ip=ip) #naming scheme
+
+    print 'switches'
+    #likely only works for L=2
+    for i in range(L, -1, -1):
+        num_groups = 2 * p ** (L - i)
+        num_switches_per_group = p ** i
+
+        # add nodes for L2
+        if i == L:
+            for j in range(n_switches_per_layer / 2): # for each switch on L2
+                switch_num = i * num_groups * num_switches_per_group + j
+                ip = socket.inet_ntoa(struct.pack('!L', 10 << 24 | i << 8 | j))
+                switch_to_ip['s' + str(switch_num)] = ip
+                G.add_node('s' + str(switch_num), ip=ip)
+        else:
+            for m in range(num_groups): # group num
+                for j in range(num_switches_per_group): # index within group
+                    switch_num = i * num_groups * num_switches_per_group + m * num_switches_per_group + j
+                    loc = m * p ** i
+                    ip = socket.inet_ntoa(struct.pack('!L', 10 << 24 | loc << 10 | i << 8 | j))
+                    switch_to_ip['s' + str(switch_num)] = ip
+                    G.add_node('s' + str(switch_num), ip=ip) #update naming scheme once figured
+
+                    if i == 0: # from L0 to servers
+                        for n in range(num_servers_per_switch):
+                            curr_switch = 's' + str(switch_num)
+                            curr_host = 'h' + str(switch_num * num_servers_per_switch + n)
+                            G.add_edge(curr_switch, curr_host)
+                            outport_mappings[(curr_switch, curr_host)] = open_ports[switch_num]
+                            outport_mappings[(curr_host, curr_switch)] = 1
+                            open_ports[switch_num] -= 1
+
+                    for n in range(p):
+                        if i == 0: #L0 to L1
+                            to_switch_num = (switch_num / p) * p + n_switches_per_layer * (i + 1) + n
+                        else: #L1 to L2
+                            if m % 2 == 0: #A subtree
+                                to_switch_num = (i + 1) * n_switches_per_layer + j * p + n
+                            else: #B subtree
+                                to_switch_num = (i + 1) * n_switches_per_layer + n * p ** i + j
+                        from_switch = 's' + str(switch_num)
+                        to_switch = 's' + str(to_switch_num)
+                        G.add_edge(from_switch, to_switch)
+                        outport_mappings[(from_switch, to_switch)] = open_ports[switch_num]
+                        outport_mappings[(to_switch, from_switch)] = open_ports[to_switch_num]
+                        open_ports[switch_num] -= 1
+                        open_ports[to_switch_num] -= 1
     return topo
+                            
+                        
 
-
-'''
-# TODO: move somewhere else
-class JellyFishTop(Topo):
-
-    def __init__(self, topo_map):
-        self.topo_map = topo_map
-
-    def build(self):
-        topo_map = self.topo_map
-        mn_hosts = []
-        for h in topo_map["hosts"]:
-            mn_hosts.append(self.addHost(h))
-
-        mn_switches = []
-        for i, s in enumerate(topo_map["switches"]):
-            switch = self.addSwitch(s)
-            mn_switches.append(switch)
-            if i < len(topo_map["hosts"]):
-                self.addLink(mn_hosts[i], switch)
-
-        for p in topo_map["link_pairs"]:
-            self.addLink(mn_switches[p[0]], mn_switches[p[1]])
-'''
+    
 
